80c92146044d8f4391a7050e9950bc9eac2d1b61
DerekNester <derekn@wustl.edu>
2026-04-20 16:14:26 -0500

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] inDeg = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i=0; i <numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] p : prerequisites) {
+            adj.get(p[1]).add(p[0]);
+            inDeg[p[0]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i=0; i<numCourses;i++) if (inDeg[i] ==0) q.add(i);
+        int[] order = new int[numCourses];
+        int idx=0;
+        while(!q.isEmpty()) {
+            int u = q.poll();
+            order[idx++] = u;
+            for(int v:adj.get(u)) {
+                if (--inDeg[v]==0) q.add(v);
+            }
+        }
+        if (idx == numCourses) return order;
+        else return new int[0];
+        
+    }
+}
